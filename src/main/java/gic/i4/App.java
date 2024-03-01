d89bfc13e78e562663482810c2f40ad34a167012
sudo12 <rakinchann305@gmail.com>
2024-03-01 10:50:26 +0700

    update

--- src/main/java/gic/i4/App.java
+++ src/main/java/gic/i4/App.java
@@ -4,10 +4,8 @@ package gic.i4;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Welcome guys!");
     }
 }

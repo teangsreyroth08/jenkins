c93a4fd3dbad5597a612605b9a95b6e478a597b0
sudo12 <teangsreyroth80@gmail.com>
2024-03-29 10:47:06 +0700

    build

---
+++ src/test/java/mitderm/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package mitderm.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
